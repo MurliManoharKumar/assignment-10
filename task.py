40a2b9961982f44bdf7b8fec6e615766924c6b6e
Sreejit Hore <sreejit2006hore@gmail.com>
2026-03-03 16:20:00 +0530

    modified the previous fn

--- task.py
+++ task.py
@@ -1,4 +1,4 @@
-tasks = [] def display_tasks(): if not tasks: print("No tasks yet.") for i, task in enumerate(tasks, 1): print(f" {i}. {task}")
+tasks = [] def display_tasks(): if not tasks: print("No tasks yet.") for i, task in enumerate(tasks, 1): print(f" [{i}] {task}")
 def delete_task(task_index):
  if 0 <= task_index < len(tasks): 
  removed = tasks.pop(task_index)
