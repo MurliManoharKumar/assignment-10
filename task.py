fa20fca196ebb95d17c6831686b9a67e6a3c8780
Murli Manohar <murlimanohar1098@gmail.com>
2026-03-03 11:32:51 +0530

    Second commit in feature branch

--- task.py
+++ task.py
@@ -1,3 +1,4 @@
 tasks = [] def display_tasks(): if not tasks: print("No tasks yet.") for i, task in enumerate(tasks, 1): print(f" {i}. {task}")
 
 def add_task(task_name): tasks.append(task_name) print(f"✅ Task added: {task_name}")
+Added more content in feature branch
